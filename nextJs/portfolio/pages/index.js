aedc13203c0fbe4109a7633900e92fc0156f3aa7
wkoslik <wkoslik@gmail.com>
2021-03-10 19:20:27 -0500

    adding header information

--- nextJs/portfolio/pages/index.js
+++ nextJs/portfolio/pages/index.js
@@ -1,9 +1,11 @@
+import Header from './partials/Header'
 import About from './about'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
+    <Header />
     <About />
     </div>
   )
--- nextJs/portfolio/pages/partials/Header.js
+++ nextJs/portfolio/pages/partials/Header.js
@@ -1,7 +1,15 @@
 export default function Header(){
     return(
         <header>
-            
+            <img src='/initials header.png' alt="Whitney Kosliks initials- wk"/>
+            <h1>Whitney Koslik</h1>
+            <navbar><ul>
+                <li>Resume</li>
+                <li>About</li>
+                <li>Projects</li>
+                <li>Recommendations</li>
+                <li>Connect</li>
+            </ul></navbar>
         </header>
     )
 }
