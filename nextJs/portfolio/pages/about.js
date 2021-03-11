758f3dc34f18f0f10df96a785bf544b713a86097
wkoslik <wkoslik@gmail.com>
2021-03-10 19:14:02 -0500

    creating nextjs app

---
+++ nextJs/portfolio/pages/about.js
@@ -0,0 +1,10 @@
+
+export default function About() {
+    return(
+        <div>
+            <img src='/whitneyKoslik.jpg' alt="Whitney Koslik"/>
+            <h2>Hi! I'm <span className="turn-pink">Whitney</span>.</h2>
+            <p>I am a full stack web developer who focuses on creative solutions through active communication, adaptability, and critical thinking.</p>
+        </div>
+    )
+}
---
+++ nextJs/portfolio/pages/index.js
@@ -0,0 +1,10 @@
+import About from './about'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+    <About />
+    </div>
+  )
+}
