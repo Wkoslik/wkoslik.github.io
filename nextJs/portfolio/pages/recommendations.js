7175d11c61d692a1500b196dab49d4ceead38a0c
wkoslik <wkoslik@gmail.com>
2021-03-29 14:58:09 -0400

    recommendations

---
+++ nextJs/portfolio/pages/recommendations.js
@@ -0,0 +1,10 @@
+export default function Recommendation(props){
+    return(
+        <div className="rec">
+            <img></img>
+            <h4>Name</h4>
+            <h5>title</h5>
+            <p>Recommendation</p>
+        </div>
+    )
+}
---
+++ nextJs/portfolio/pages/recommendationscontainer.js
@@ -0,0 +1,12 @@
+import { Recommendations } from '../Recommendations.json'
+import Recommendation from './recommendations.js'
+
+export default function RecommendationsContainer() {
+
+
+    return (
+        <div>
+            <h1>Recommendations</h1>
+        </div>
+    )
+}
