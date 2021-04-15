50bd24c74a05c36f05114a16cd08e0a8e845ccab
wkoslik <wkoslik@gmail.com>
2021-04-15 10:29:13 -0400

    adding forbidden island

---
+++ nextJs/portfolio/pages/forbidden_island.js
@@ -0,0 +1,10 @@
+const ForbiddenIsland = (props) => {
+    return (
+        <div>
+        <h1>Forbidden Island</h1>
+        <a href="https://wkoslik.github.io/forbidden_island/">Enter</a>
+        </div>
+    )
+}
+
+export default ForbiddenIsland
